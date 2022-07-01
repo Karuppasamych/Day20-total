e26dc6fffc90b8cc52adae9bf9cde03e4e073c36
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-07-01 23:43:42 +0530

    Create total.js

---
+++ total.js
@@ -0,0 +1,7 @@
+let tamil = Number(prompt('Enter the tamil mark'));
+let english = Number(prompt('Enter the English mark'));
+let maths = Number(prompt('Enter the maths mark'));
+let science = Number(prompt('Enter the science mark'));
+let social =Number(prompt('Enter the social mark'));
+let total = tamil + english + maths + science + social
+console.log("Total value = ",total);
